93aa3acf5bb8532d3c8fa7fda74338a2a3c43e5f
aditya-sutar-45 <aditya1.sutar@outlook.com>
2025-10-06 22:47:13 +0530

    react-router installed

---
+++ frontend/src/components/test.jsx
@@ -0,0 +1,11 @@
+function Test() {
+  return (
+    <>
+      <h1>this is a test page</h1>
+      <p>ugga bugga this is something</p>
+      <button className="btn btn-primary">click me please</button>
+    </>
+  );
+}
+
+export default Test;
