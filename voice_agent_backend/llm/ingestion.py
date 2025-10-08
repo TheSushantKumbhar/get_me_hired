45fafdd45274b4209f4d4451a3cc397181ccf97f
aditya-sutar-45 <aditya1.sutar@outlook.com>
2025-10-08 10:35:22 +0530

    can now ingest documment to pinecone

--- voice_agent_backend/llm/ingestion.py
+++ voice_agent_backend/llm/ingestion.py
@@ -1,11 +1,19 @@
 from glob import glob
 import os
+from dotenv import load_dotenv
 from langchain_community.document_loaders import TextLoader
 from langchain.text_splitter import MarkdownHeaderTextSplitter
+from langchain_ollama import OllamaEmbeddings
+from langchain_core import embeddings
+from langchain_pinecone import PineconeVectorStore
+
+load_dotenv()
 
 all_docs = []
 headers_to_split = [("###", "question")]
 md_paths = glob("../llm/data/mrkdown/*.md")
+
+embeddings = OllamaEmbeddings(model="mxbai-embed-large")
 
 print("loading markdown files")
 for markdown_path in md_paths:
@@ -29,13 +37,20 @@ for markdown_path in md_paths:
         doc.metadata["answer"] = answer
 
     # for testing delete later
-    for doc in docs:
-        print("language: ", doc.metadata["language"])
-        print("question: ", doc.page_content)
+    # for doc in docs:
+    # print("language: ", doc.metadata["language"])
+    # print("question: ", doc.page_content)
     # it works (hopefully)
 
     all_docs.append(docs)
 
-print(len(all_docs))
 # documents (markdown files) loaded
 print("markdown files loaded!")
+all_docs = [doc for sublist in all_docs for doc in sublist]
+print(len(all_docs))
+
+print("ingesting....")
+PineconeVectorStore.from_documents(
+    all_docs, embeddings, index_name=os.environ["INDEX_NAME"]
+)
+print("finished ingesting the documents....")
