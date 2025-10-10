53c894ff48050b69edac9b80e31d6035971adb0a
aditya-sutar-45 <aditya1.sutar@outlook.com>
2025-10-11 00:03:27 +0530

    mock landing page added

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,3 +1,22 @@
-function LandingPage() {}
+import { ArrowRight } from "lucide-react";
+
+function LandingPage() {
+  return (
+    <div className="hero bg-base-200 min-h-screen font-work-sans">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold font-space-mono">Get-Me-Hired</h1>
+          <p className="py-6">
+            Get ready for your next interview with guided practice sessions and
+            practical insights to help you improve.
+          </p>
+          <button className="btn btn-primary">
+            Browse Jobs <ArrowRight />{" "}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default LandingPage;
