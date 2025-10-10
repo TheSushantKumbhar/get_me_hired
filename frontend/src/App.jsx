60036e36a7588db92f7ff38fd09bfc50051ce6e1
aditya-sutar-45 <aditya1.sutar@outlook.com>
2025-10-10 23:56:10 +0530

    added navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import InterviewRoom from "./pages/InterviewRoom.jsx";
 import LandingPage from "./pages/LandingPage.jsx";
+import Layout from "./Layout.jsx";
 
 const router = createBrowserRouter([
-  { path: "/", element: <LandingPage /> },
+  {
+    element: <Layout />,
+    children: [{ path: "/", element: <LandingPage /> }],
+  },
   { path: "/interview", element: <InterviewRoom /> },
 ]);
 
