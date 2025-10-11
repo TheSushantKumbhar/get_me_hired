01b46d3e93f244c370f8c2cd0b839cc4b877b1f5
aditya-sutar-45 <aditya1.sutar@outlook.com>
2025-10-11 18:48:51 +0530

    added cors

--- backend/main.go
+++ backend/main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/TheSushantKumbhar/get_me_hired/backend/repository"
 	"github.com/TheSushantKumbhar/get_me_hired/backend/routes"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/joho/godotenv"
 )
 
@@ -19,6 +20,12 @@ func main() {
 	}
 
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:5173, http://localhost:5000",
+		AllowHeaders: "*",
+		AllowMethods: "*",
+	}))
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello World!")
