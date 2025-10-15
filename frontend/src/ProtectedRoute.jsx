4729f2a5b6c8e217aadd5e7ac3ab0398a5383792
aditya-sutar-45 <aditya1.sutar@outlook.com>
2025-10-16 00:53:40 +0530

    added toast when user tries to access protected route without loggin

--- frontend/src/ProtectedRoute.jsx
+++ frontend/src/ProtectedRoute.jsx
@@ -1,11 +1,21 @@
 import { Navigate, replace } from "react-router-dom";
 import { useAuth } from "./contexts/AuthContext";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
 function ProtectedRoute({ children }) {
   const { user, loading } = useAuth();
+  const [redirect, setRedirect] = useState(false);
+
+  useEffect(() => {
+    if (!loading && !user) {
+      toast.error("you must be logged in to access this page!");
+      setRedirect(true);
+    }
+  }, [loading, user]);
 
   if (loading) return <p>Loading.....</p>;
-  if (!user) return <Navigate to="/" replace />;
+  if (redirect) return <Navigate to="/" replace />;
 
   return children;
 }
--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,33 +1,37 @@
 import { ArrowRight } from "lucide-react";
+import { Toaster } from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 
 function LandingPage() {
   const navigate = useNavigate();
 
   return (
-    <div className="hero bg-base-200 min-h-[95vh] font-work-sans">
-      <div className="hero-content text-center">
-        <div className="max-w-md">
-          <h1 className="text-6xl font-bold font-space-mono">Get Me Hired</h1>
-          <p className="py-6">
-            Get ready for your next interview with guided practice sessions and
-            practical insights to help you improve.
-          </p>
-          <button
-            className="btn btn-primary italic m-1"
-            onClick={() => navigate("/job")}
-          >
-            Browse Jobs <ArrowRight />{" "}
-          </button>
-          <button
-            className="btn btn-secondary text-secondary-content italic m-1"
-            onClick={() => navigate("/interview")}
-          >
-            Try Interview <ArrowRight />{" "}
-          </button>
+    <>
+      <Toaster />
+      <div className="hero bg-base-200 min-h-[95vh] font-work-sans">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <h1 className="text-6xl font-bold font-space-mono">Get Me Hired</h1>
+            <p className="py-6">
+              Get ready for your next interview with guided practice sessions
+              and practical insights to help you improve.
+            </p>
+            <button
+              className="btn btn-primary italic m-1"
+              onClick={() => navigate("/job")}
+            >
+              Browse Jobs <ArrowRight />{" "}
+            </button>
+            <button
+              className="btn btn-secondary text-secondary-content italic m-1"
+              onClick={() => navigate("/interview")}
+            >
+              Try Interview <ArrowRight />{" "}
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
