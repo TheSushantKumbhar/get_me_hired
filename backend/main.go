15fc98b55e2107ee4dd6f2d626416b7bbbe3793c
Sushant Kumbhar <me.sushantkumbhar@gmail.com>
2025-10-15 16:11:52 +0530

    changed cors

--- backend/main.go
+++ backend/main.go
@@ -22,7 +22,7 @@ func main() {
 	app := fiber.New()
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "http://localhost:5173",
+		AllowOrigins:     "http://localhost:5173, http://localhost:5174",
 		AllowMethods:     "*",
 		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
 		AllowCredentials: true,
