93c45e3affda22d97a0cc012f4c07bb7ec4a4711
aditya-sutar-45 <aditya1.sutar@outlook.com>
2025-10-17 16:10:49 +0530

    able to select room languages for code editor + added output window

--- frontend/src/components/InterviewRoom/CodeEditor.jsx
+++ frontend/src/components/InterviewRoom/CodeEditor.jsx
@@ -1,7 +1,29 @@
 import { Editor } from "@monaco-editor/react";
-import { Code } from "lucide-react";
+import { Code, Play } from "lucide-react";
+import { useEffect, useRef, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 function CodeEditor() {
+  const location = useLocation();
+
+  const editorRef = useRef();
+  const [codeValue, setCodeValue] = useState("");
+  const [language, setLanguage] = useState(
+    localStorage.state?.jobData.languages[0] || "javascript",
+  );
+
+  const languages = location.state?.jobData.languages || ["javascript"];
+
+  const [toggleEditor, setToggleEditor] = useState(true);
+
+  const onMount = (editor) => {
+    editorRef.current = editor;
+  };
+
+  const handleCodeSubmit = () => {
+    console.log(codeValue);
+  };
+
   return (
     <div className="drawer">
       <input id="my-drawer-1" type="checkbox" className="drawer-toggle" />
@@ -18,17 +40,64 @@ function CodeEditor() {
           aria-label="close sidebar"
           className="drawer-overlay"
         ></label>
-        <ul className="menu bg-base-200 min-h-full w-80 p-4">
-          <div className="h-[5vh] w-[60vw] bg-base-200 flex justify-end">
-            <button className="btn btn-success">Submit</button>
+        <ul className="menu bg-none min-h-full">
+          <div className="h-[5vh] w-[60vw] bg-base-200 flex justify-end gap-2">
+            <button className="btn btn-warning">
+              <Play /> Run
+            </button>
+            <button className="btn btn-success" onClick={handleCodeSubmit}>
+              Submit
+            </button>
           </div>
-          <Editor
-            height="95vh"
-            width="60vw"
-            theme="vs-dark"
-            defaultLanguage="javascript"
-            defaultValue="// code here"
-          />
+          <div className="h-[5vh] w-[60vw] bg-base-200 flex justify-between">
+            <div>
+              <button
+                className="btn btn-soft"
+                onClick={() => {
+                  setToggleEditor((prev) => !prev);
+                }}
+                disabled={toggleEditor}
+              >
+                Editor
+              </button>
+              <button
+                className="btn btn-soft"
+                onClick={() => {
+                  setToggleEditor((prev) => !prev);
+                }}
+                disabled={!toggleEditor}
+              >
+                Output
+              </button>
+            </div>
+            <div>
+              <select
+                defaultValue={language}
+                className="select select-secondary"
+              >
+                {languages.map((lang, i) => (
+                  <option key={i}>{lang}</option>
+                ))}
+              </select>
+            </div>
+          </div>
+          {toggleEditor === true ? (
+            <Editor
+              height="90vh"
+              width="60vw"
+              onMount={onMount}
+              theme="vs-dark"
+              defaultLanguage={language}
+              language={language}
+              defaultValue=""
+              value={codeValue}
+              onChange={(value) => setCodeValue(value)}
+            />
+          ) : (
+            <div className="h-[90vh] w-[60vw] bg-base-100">
+              <h1 className="text-2xl font-work-sans">Output</h1>
+            </div>
+          )}
         </ul>
       </div>
     </div>
